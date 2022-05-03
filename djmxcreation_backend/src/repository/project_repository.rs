55f5444e970bb357f9fd1aa6f41498697b60dea5
Judicaël DUBRAY <djmx972@gmail.com>
2022-05-03 23:55:50 +0200

    finsh project repository

--- djmxcreation_backend/src/repository/project_repository.rs
+++ djmxcreation_backend/src/repository/project_repository.rs
@@ -77,14 +77,66 @@ pub async fn update_visibility(project_id: i32, is_visible: bool) -> Result<(), 
         .bind(project_id)
         .execute(&mut tx)
         .await?;
-
     tx.commit().await?;
-
     Ok(())
 }
-// TODO get projects
-// TODO get project by id
-// TODO get project content by id
-// TODO get project contents
-//TODO delete project by id
-//TODO delete project content by id
+
+pub async fn get_projects() -> Result<Vec<ProjectEntity>, Error> {
+    let db = init_db().await?;
+    let sql = "SELECT * FROM project";
+    let query = sqlx::query_as::<_, ProjectEntity>(&sql);
+    let projects = query.fetch_all(&db).await?;
+    Ok(projects)
+}
+
+pub async fn get_project_by_id(id: i32) -> Result<ProjectEntity, Error> {
+    let db = init_db().await?;
+    let sql = "SELECT * FROM project where id= $1";
+    let query = sqlx::query_as::<_, ProjectEntity>(&sql).bind(id);
+    let project = query.fetch_one(&db).await?;
+    Ok(project)
+}
+
+pub async fn get_projects_contents(project_id: i32) -> Result<Vec<ProjectContentEntity>, Error> {
+    let db = init_db().await?;
+    let sql = "SELECT * FROM project_content where project_id = $1";
+    let query = sqlx::query_as::<_, ProjectContentEntity>(&sql).bind(project_id);
+    let contents = query.fetch_all(&db).await?;
+    Ok(contents)
+}
+
+pub async fn get_projects_content_by_id(
+    project_id: i32,
+    id: i32,
+) -> Result<ProjectContentEntity, Error> {
+    let db = init_db().await?;
+    let sql = "SELECT * FROM project_content where project_id = $1 and id= $2";
+    let query = sqlx::query_as::<_, ProjectContentEntity>(&sql)
+        .bind(project_id)
+        .bind(id);
+    let content = query.fetch_one(&db).await?;
+    Ok(content)
+}
+
+pub async fn delete_project_by_id(project_id: i32) -> Result<(), Error> {
+    let db = init_db().await?;
+    let mut tx = db.begin().await?;
+    sqlx::query("DELETE project WHERE id = $1 ")
+        .bind(project_id)
+        .execute(&mut tx)
+        .await?;
+    tx.commit().await?;
+    Ok(())
+}
+
+pub async fn delete_project_content_by_id(project_id: i32, id: i32) -> Result<(), Error> {
+    let db = init_db().await?;
+    let mut tx = db.begin().await?;
+    sqlx::query("DELETE project_content WHERE id = $1 and project_id=$2 ")
+        .bind(id)
+        .bind(project_id)
+        .execute(&mut tx)
+        .await?;
+    tx.commit().await?;
+    Ok(())
+}
--- sql/02-CREATE_TABLE_PROJECT.sql
+++ sql/02-CREATE_TABLE_PROJECT.sql
@@ -11,5 +11,6 @@ CREATE TABLE project_content(
     id serial PRIMARY KEY,
     content jsonb,
     project_id INT NOT NULL,
+    created_on TIMESTAMP NOT NULL,
     CONSTRAINT fk_project FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE
 );
