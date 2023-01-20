87da821c72aae124d0a603fff0a46b7df4b4d78e
Judicaël DUBRAY <djmx972@gmail.com>
2023-01-20 20:19:37 +0100

    fix data for view

--- app_core/src/view/project_view.rs
+++ app_core/src/view/project_view.rs
@@ -12,8 +12,8 @@ pub struct ProjectView {
     pub metadata: Option<MetadataDto>,
     pub description: Option<Value>,
     pub visible: bool,
-    // created_on: Option<chrono::DateTime<chrono::Utc>>,
-    // updated_on: Option<chrono::DateTime<chrono::Utc>>,
+    created_on: Option<chrono::DateTime<chrono::Utc>>,
+    updated_on: Option<chrono::DateTime<chrono::Utc>>,
     pub contents: Vec<ContentView>,
 }
 
@@ -24,6 +24,8 @@ impl ProjectView {
         description: Option<Value>,
         visible: bool,
         contents: Vec<ContentView>,
+        created_on: Option<chrono::DateTime<chrono::Utc>>,
+        updated_on: Option<chrono::DateTime<chrono::Utc>>,
     ) -> Self {
         Self {
             id,
@@ -31,6 +33,8 @@ impl ProjectView {
             description,
             visible,
             contents,
+            created_on,
+            updated_on,
         }
     }
 }
@@ -38,6 +42,14 @@ impl ProjectView {
 impl From<ProjectDto> for ProjectView {
     fn from(dto: ProjectDto) -> Self {
         let contents: Vec<ContentView> = vec![];
-        Self::new(dto.id, dto.metadata, dto.description, dto.visible, contents)
+        Self::new(
+            dto.id,
+            dto.metadata,
+            dto.description,
+            dto.visible,
+            contents,
+            dto.created_on,
+            dto.updated_on,
+        )
     }
 }
