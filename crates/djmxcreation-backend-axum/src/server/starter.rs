e2e4d6d1732006fa19f42e51d940f823e0517bc1
Judicaël DUBRAY <djmx972@gmail.com>
2025-03-07 17:55:08 +0100

    add tracing

--- crates/djmxcreation-backend-axum/src/server/starter.rs
+++ crates/djmxcreation-backend-axum/src/server/starter.rs
@@ -7,28 +7,29 @@ use std::{
 use crate::{
     router::{
         about_me_router::AboutMeRouter, contact_router::ContactRouter,
-        observability_router::ObservabilityRouter, project_router::ProjectRouter,
+        project_router::ProjectRouter,
     },
     service::service_register::ServiceRegister,
 };
-// use aide::openapi::OpenApi;
+
 use anyhow::Context;
-use app_config::{config::Config, security_config::SecurityConfig};
+use app_config::config::Config;
+use axum::body::Body;
 use axum::{
+    BoxError, Json, Router,
     error_handling::HandleErrorLayer,
     extract::MatchedPath,
-    // headers::{authorization::Basic, Authorization},
     middleware::{self, Next},
     response::{IntoResponse, Response},
     routing::get,
-    BoxError,
-    Json,
-    Router,
-    // TypedHeader,
 };
-use hyper::{header::HeaderValue, Method, Request, StatusCode};
+use hyper::{Method, Request, StatusCode};
+use metrics_exporter_prometheus::PrometheusHandle;
 use metrics_exporter_prometheus::{Matcher, PrometheusBuilder};
-// use migration::init_db_migration;
+
+use hyper::header;
+use metrics::counter;
+use metrics::histogram;
 use once_cell::sync::Lazy;
 use repository::config::{
     db::DatabasePool,
@@ -38,6 +39,10 @@ use serde_json::json;
 use tower::ServiceBuilder;
 use tower_http::{cors::CorsLayer, trace::TraceLayer};
 
+// constants for configuration
+const METRICS_PATH: &str = "/metrics";
+const HEALTH_PATH: &str = "/health";
+
 static GLOBAL_CONFIG: Lazy<Arc<Config>> = Lazy::new(|| Arc::new(Config::new()));
 
 const HTTP_TIMEOUT: u64 = 30;
@@ -45,86 +50,158 @@ const EXPONENTIAL_SECONDS: &[f64] = &[
     0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
 ];
 
-async fn handle_timeout_error(err: BoxError) -> (StatusCode, Json<serde_json::Value>) {
-    if err.is::<tower::timeout::error::Elapsed>() {
-        (
-            StatusCode::REQUEST_TIMEOUT,
-            Json(json!({
-                "error":
-                    format!("request took longer than the configured {HTTP_TIMEOUT} second timeout")
-            })),
-        )
-    } else {
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            Json(json!({
-                "error": format!("unhandled internal error: {err}")
-            })),
-        )
+pub fn init_logger() {
+    let filter = EnvFilter::new("info");
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .json()
+        .init();
+}
+
+async fn push_log(level: &str, message: &str) {
+    let client = Client::new();
+    let payload = json!({
+        "level": level,
+        "message": message,
+        "timestamp": chrono::Utc::now().to_rfc3339()
+    });
+
+    if let Err(e) = client
+        .post("http://fluent-bit:9880/logs") //to modify
+        .json(&payload)
+        .send()
+        .await
+    {
+        error!("Failed to send log: {}", e);
     }
 }
 
-// pub async fn auth<B>(
-//     // run the `TypedHeader` extractor
-//     TypedHeader(auth): TypedHeader<Authorization<Basic>>,
-//     // you can also add more extractors here but the last
-//     // extractor must implement `FromRequest` which
-//     // `Request` does
-//     request: Request<B>,
-//     next: Next<B>,
-// ) -> Result<Response, StatusCode> {
-//     if token_is_valid(auth.0) {
-//         let response = next.run(request).await;
-//         Ok(response)
-//     } else {
-//         Err(StatusCode::UNAUTHORIZED)
-//     }
-// }
+async fn track_metrics(req: Request<Body>, next: Next) -> Response {
+    let path = if let Some(matched_path) = req.extensions().get::<MatchedPath>() {
+        matched_path.as_str().to_owned()
+    } else {
+        req.uri().path().to_owned()
+    };
 
-// fn token_is_valid(token: Basic) -> bool {
-//     let SecurityConfig { username, password } = GLOBAL_CONFIG.clone().get_security();
+    let method = req.method().clone();
+    let start = Instant::now();
 
-//     username == token.username() && password == token.password()
-// }
+    let response = next.run(req).await;
+
+    let latency = start.elapsed().as_secs_f64();
+    let status = response.status().as_u16().to_string();
+
+    let log_level = if status >= 500 {
+        "error"
+    } else if status >= 400 {
+        "warn"
+    } else {
+        "info"
+    };
+
+    let message = format!(
+        "Request [{} {}] - {} - {:.3}s",
+        method, path, status, latency
+    );
+
+    push_log(log_level, &message).await;
+
+    let _ = histogram!(
+        "http_requests_duration_seconds",
+        "latency"=>latency.to_string(),
+        "path" => path.clone(),
+        "method" => method.to_string(),
+        "status" => status.clone()
+    );
+
+    let counter = counter!(
+        "http_requests_total",
+        "path" => path,
+        "method" => method.to_string(),
+        "status" => status
+    );
+
+    counter.increment(1);
+
+    response
+}
+
+async fn handle_timeout_error(err: BoxError) -> (StatusCode, Json<serde_json::Value>) {
+    let error_message = if err.is::<tower::timeout::error::Elapsed>() {
+        format!("Request Timeout: {} seconds", HTTP_TIMEOUT)
+    } else {
+        format!("Unhandled Error: {}", err)
+    };
+
+    push_log("error", &error_message).await;
+
+    (
+        StatusCode::INTERNAL_SERVER_ERROR,
+        Json(json!({ "error": error_message })),
+    )
+}
 
 pub async fn start() -> anyhow::Result<()> {
-    // aide::gen::on_error(|error| {
-    //     println!("{error}");
-    // });
-
-    // aide::gen::extract_schemas(true);
-
-    // let mut api = OpenApi::default();
-
     let config = GLOBAL_CONFIG.clone();
 
-    // init_db_migration(&config.database)
-    //     .await
-    //     .expect("Failed to migrate database");
+    // Initialize database
+    let client_db = DatabasePool::new(&config.database, None)
+        .await
+        .context("Failed to initialize database pool")?;
 
-    let client_db = DatabasePool::new(&config.database, None).await?;
-
+    // Initialize storage
     let storage = config.clone().get_storage();
+    let storage_client =
+        get_aws_client(storage).context("Failed to create object Storage client")?;
+    create_bucket("portfolio", storage_client.clone())
+        .await
+        .context("Failed to create bucket")?;
 
-    let storage_client = get_aws_client(storage).expect("Failed to create object Storage client");
-
-    create_bucket("portfolio", storage_client.clone()).await?;
-
+    // Initialize services
     let service_register = ServiceRegister::new(Arc::new(client_db), storage_client);
 
-    let recorder_handle = PrometheusBuilder::new()
-        .set_buckets_for_metric(
-            Matcher::Full(String::from("http_requests_duration_seconds")),
-            EXPONENTIAL_SECONDS,
-        )
-        .context("could not setup buckets for metrics, verify matchers are correct")?
-        .install_recorder()
-        .context("could not install metrics recorder")?;
+    // Setup metrics
+    let recorder_handle = setup_metrics()?;
 
-    // let my_auth = MyAuth { basic_auth };
+    // Create router with all middlewares
+    let app = create_router(service_register, recorder_handle)?;
 
-    let router = Router::new()
-        .nest("/", ObservabilityRouter::new_router())
+    // Start server
+    let addr = format!("0.0.0.0:{}", &config.port);
+    let listener = tokio::net::TcpListener::bind(&addr)
+        .await
+        .context(format!("Failed to bind to {}", addr))?;
+
+    println!("Server listening on {}", addr);
+    axum::serve(listener, app.into_make_service()).await?;
+    Ok(())
+}
+
+fn create_router(
+    service_register: ServiceRegister,
+    metrics_handler: PrometheusHandle,
+) -> anyhow::Result<Router> {
+    let cors = CorsLayer::new()
+        // .allow_origin(
+        //     config
+        //         .allowed_origins
+        //         .iter()
+        //         .map(|o| o.parse::<HeaderValue>().unwrap())
+        //         .collect::<Vec<_>>(),
+        // )
+        .allow_methods([
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::DELETE,
+            Method::OPTIONS,
+        ])
+        .allow_headers([header::CONTENT_TYPE, header::AUTHORIZATION, header::ACCEPT])
+        .max_age(Duration::from_secs(3600));
+
+    let app = Router::new()
+        .route(HEALTH_PATH, get(health_check))
+        .route(METRICS_PATH, get(move || ready(metrics_handler.render())))
         .nest(
             "/api/about",
             AboutMeRouter::new_router(service_register.clone()),
@@ -137,32 +214,29 @@ pub async fn start() -> anyhow::Result<()> {
             "/api/contact",
             ContactRouter::new_router(service_register.clone()),
         )
-        .route("/metrics", get(move || ready(recorder_handle.render())))
         .layer(
             ServiceBuilder::new()
                 .layer(TraceLayer::new_for_http())
                 .layer(HandleErrorLayer::new(handle_timeout_error))
-                .timeout(Duration::from_secs(HTTP_TIMEOUT)),
+                .timeout(Duration::from_secs(HTTP_TIMEOUT))
+                .layer(middleware::from_fn(track_metrics))
+                .layer(cors),
+        );
+
+    Ok(app)
+}
+
+fn setup_metrics() -> anyhow::Result<PrometheusHandle> {
+    PrometheusBuilder::new()
+        .set_buckets_for_metric(
+            Matcher::Full(String::from("http_requests_duration_seconds")),
+            EXPONENTIAL_SECONDS,
         )
-        .layer(CorsLayer::permissive())
-        .layer(TraceLayer::new_for_http());
-    // .layer(
-    //     CorsLayer::new()
-    //         .allow_origin(Any) //TODO to modify
-    //         // .allow_origin(["http://localhost:3008".parse::<HeaderValue>().unwrap()]) //TODO to modify
-    //         .allow_methods([
-    //             Method::GET,
-    //             Method::POST,
-    //             Method::PUT,
-    //             Method::DELETE,
-    //             Method::OPTIONS,
-    //         ]),
-    // )
-    // .route_layer(middleware::from_fn(track_metrics));
-    // .layer(middleware::from_fn(auth));
+        .context("Failed to setup metrics buckets")?
+        .install_recorder()
+        .context("Failed to install metrics recorder")
+}
 
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", &config.port)).await?;
-
-    axum::serve(listener, router.into_make_service()).await?;
-    Ok(())
+async fn health_check() -> impl IntoResponse {
+    StatusCode::OK
 }
